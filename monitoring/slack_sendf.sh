e0d08462a6d02341b69253beeae13e42aae5c60c
Roman <24roman12@gmail.com>
2020-03-28 20:34:51 +0100

    Slack monitoring notifications - webhook + photo send

---
+++ monitoring/slack_sendf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. $(dirname "$0")/.psikuta
+
+co_wyslac=$(find /kamery2TB/$(date +'%Y-%m-%d')_$1/ -iname "*_$2*" | head -n1)
+echo "Znalazlem taki plik: $co_wyslac"
+if [[ $co_wyslac != $(cat /tmp/slack_sendf.lock) ]]; then
+  echo "To jest nowe zdarzenie - wysylam zdjecie na Slacka"
+  curl -F file=@$co_wyslac -F "initial_comment=Jeden z kilku obrazów z tego wydarzenia wykrycia ruchu poniżej:" -F channels=C010EGWMZGC -H "Authorization: Bearer $slack_token_wysylanie" https://slack.com/api/files.upload
+fi
+echo $co_wyslac > /tmp/slack_sendf.lock 
---
+++ monitoring/slack_webh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+. $(dirname "$0")/.psikuta
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#kamery-monitoring\", \"username\": \"Magazyn\", \"text\": \"<@aleksander.sokalski> Wykryłem ruch w obszarze monitoringu: $1 ( id=$2 )\", \"icon_emoji\": \":flashlight:\"}" $slack_webhook_mag
+
+/etc/motioneye/slack_sendf.sh $1 $2
