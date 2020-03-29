f5552bff29ab7a2e41cf4c6686f4bf45c630a4c0
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-03-29 17:59:45 +0200

    Update raport_brak_netu.sh

--- raport_brak_netu.sh
+++ raport_brak_netu.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-echo "Ponizej raport wystepowania zanikow dostawy internetu pod adresem Maslomiaca, ul. Modrzewiowa 2a"
+echo "Ponizej raport wystepowania zanikow dostawy internetu pod adresem ***************"
 cat /var/log/syslog*1 /var/log/syslog |  grep "cannot ping address 8.8.8.8" |  awk '{print "Dnia "$1" "$2", godz "$3}' | awk -F':' '{print $1":"$2}' | sort -k3 -r
 zcat /var/log/syslog*gz |  grep "cannot ping address 8.8.8.8" | sort -r | awk '{print "Dnia "$1" "$2", godz "$3}' | awk -F':' '{print $1":"$2}'
 
