3eba9602477d80b8360e48ff1559642867bedddb
Roman <24roman12@gmail.com>
2020-03-29 07:29:43 +0200

    plick locka byl poza widocznoscia kontenera

--- monitoring/slack_sendf.sh
+++ monitoring/slack_sendf.sh
@@ -4,8 +4,8 @@
 
 co_wyslac=$(find /kamery2TB/$(date +'%Y-%m-%d')_$1/ -iname "*_$2*" | head -n1)
 echo "Znalazlem taki plik: $co_wyslac"
-if [[ $co_wyslac != $(cat /tmp/slack_sendf.lock) ]]; then
+if [[ $co_wyslac != $(cat /etc/motioneye/slack_sendf.lock) ]]; then
   echo "To jest nowe zdarzenie - wysylam zdjecie na Slacka"
   curl -F file=@$co_wyslac -F "initial_comment=Jeden z kilku obrazów z tego wydarzenia wykrycia ruchu poniżej:" -F channels=C010EGWMZGC -H "Authorization: Bearer $slack_token_wysylanie" https://slack.com/api/files.upload
 fi
-echo $co_wyslac > /tmp/slack_sendf.lock 
+echo $co_wyslac > /etc/motioneye/slack_sendf.lock 
