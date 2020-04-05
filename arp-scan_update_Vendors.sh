40b02d3a8618eac734369b62aec4848529743b0a
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-04-05 10:48:24 +0200

    Create arp-scan_update_Vendors.sh

---
+++ arp-scan_update_Vendors.sh
@@ -0,0 +1,12 @@
+# ########################## Update the list of vebdors
+cd /usr/share/arp-scan
+sudo get-iab -v -u http://standards.ieee.org/develop/regauth/iab/iab.txt
+sudo get-oui -v -u http://standards.ieee.org/develop/regauth/oui/oui.txt
+
+# ######################### If the above will not work- use second method:
+# cd /usr/share/arp-scan
+# sudo wget http://standards.ieee.org/develop/regauth/oui/oui.txt
+# sudo wget http://standards.ieee.org/develop/regauth/iab/iab.txt
+
+# ######################### Examlpe usage
+# arp-scan -l --interface enxb827eb2ed20b --timeout 5
