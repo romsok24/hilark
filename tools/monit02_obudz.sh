747d208d1f92b3bf379d965be84132ed69a163b6
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-05-25 15:00:13 +0200

    Update monit02_obudz.sh

--- tools/monit02_obudz.sh
+++ tools/monit02_obudz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-x=`ping -c1 192.168.10.13 2>&1 | grep -i unreachable`
+x=`ping -c1 192.168.10.13 2>&1 | grep -i "100% packet loss"`
 if [ ! "$x" = "" ]; then
         logger "Serwer monit02 jest down! Probuje obudzic."
         echo  "Serwer monit02 jest down! Probuje obudzic."
