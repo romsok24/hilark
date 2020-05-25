4ae94669e34976796e47c34f7108428103e173c8
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-05-25 14:54:37 +0200

    Update slack_webh.sh

--- monitoring/slack_webh.sh
+++ monitoring/slack_webh.sh
@@ -2,6 +2,6 @@
 
 . $(dirname "$0")/.psikuta
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#kamery-monitoring\", \"username\": \"Magazyn\", \"text\": \"<@aleksander.sokalski> Wykryłem ruch w obszarze monitoringu: $1 ( id=$2 )\", \"icon_emoji\": \":flashlight:\"}" $slack_webhook_mag
+curl -X POST --data-urlencode "payload={\"channel\": \"#kamery-monitoring\", \"username\": \"Magazyn\", \"text\": \"<@imie.nazwisk> Wykryłem ruch w obszarze monitoringu: $1 ( id=$2 )\", \"icon_emoji\": \":flashlight:\"}" $slack_webhook_mag
 
 /etc/motioneye/slack_sendf.sh $1 $2
