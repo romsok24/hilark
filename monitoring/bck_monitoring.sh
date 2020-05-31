7d3f6442f97f554f380dd603816d56684011f367
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-05-31 12:20:23 +0200

    Update bck_monitoring.sh

--- monitoring/bck_monitoring.sh
+++ monitoring/bck_monitoring.sh
@@ -16,12 +16,14 @@ false
 while [ $? -ne 0 -a $i -lt $MAX_RETRIES ]
 do
  i=$(($i+1))
- /usr/bin/rsync -Prvl --progress --update 10.x.y.z:/kamery2TB/$(date +"%y")-$(date +"%m")* /bck_monitoring/
+ /usr/bin/rsync -Prvl --timeout=120 --progress --update 10.9.0.6:/kamery2TB/2020-$(date +"%m")* /bck_monitoring/
+ logger "RSYNC: wykonuje $i obieg kopii zdjec monitoringu"
 done
 
 if [ $i -eq $MAX_RETRIES ]
 then
   echo "Hit maximum number of retries of rsync, giving up."
+  logger "RSYNC: Osiagnalem maximum powtorzen. Koncze program"
 fi
 
 
