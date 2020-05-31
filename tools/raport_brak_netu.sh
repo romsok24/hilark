80c1cfe43cc915b29d9591a47e79288b7f060d95
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-05-31 06:40:24 +0200

    Update raport_brak_netu.sh

--- tools/raport_brak_netu.sh
+++ tools/raport_brak_netu.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+# Wymaga rsysloga zainstalowanego i mikrotika logujacego na niego
+
 echo "Ponizej raport wystepowania zanikow dostawy internetu pod adresem ***************"
 cat /var/log/syslog*1 /var/log/syslog |  grep "cannot ping address 8.8.8.8" |  awk '{print "Dnia "$1" "$2", godz "$3}' | awk -F':' '{print $1":"$2}' | sort -k3 -r
 zcat /var/log/syslog*gz |  grep "cannot ping address 8.8.8.8" | sort -r | awk '{print "Dnia "$1" "$2", godz "$3}' | awk -F':' '{print $1":"$2}'
