870cd9c67bb9717402650d2b68b564e3e43c4c0a
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2021-04-05 15:29:43 +0200

    Update flask_main.py
    Multiple routes

--- monitoring/flask_main.py
+++ monitoring/flask_main.py
@@ -4,9 +4,19 @@ import os
 app = Flask(__name__)
 
 @app.route('/restartmeye', methods=['GET'])
-def respond():
+def respond_restart():
     print(request.json);
-    stream = os.popen('/usr/sbin/runuser -l <username> -c "/usr/bin/docker restart <container>"')
+    stream = os.popen('/usr/sbin/runuser -l sa-motioneye -c "/usr/bin/docker restart motioneye"')
     output = stream.read()
     print(output)
+    return Response(status=200)
+
+@app.route('/kopiuj', methods=['GET'])
+def respond_kopiuj():
+    print(request.json);
+    dzien = request.args.get('dzien')
+    godzina = request.args.get('godzina')
+    stream = os.popen('echo '+ dzien +'h:' + godzina + '  > /tmp/kopiujco.txt')
+    output = stream.read()
+    print(dzien)
     return Response(status=200)
